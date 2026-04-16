737f4e15824c81c9db6d4b33d3b8f22f07070f8d
andimarfasy-glitch <andimarfasy@gmail.com>
2026-04-16 08:39:50 +0800

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php
-$nama = "andi";
+$nama = "andi m arfasy randika hidayat";
+echo "andi m arfasy randika hidayat";
+
+echo "<br><br>===================<br><br>";
+
+//OPERATOR
+$nilai1 = 5;
+$nilai2 = 98;
+$nilai3 = 9;
+$nilai4 = 10;
+$nilai5 = 12;
+
+$hasil = $nilai1 + $nilai2 - $nilai3 * $nilai4 / $nilai5;
+
+echo "Hasil dari $nilai1 + $nilai2 - $nilai3 * $nilai4 / $nilai5 adalah $hasil";
+
+
+echo "<br><br>===================<br><br>";
+
+//Percabangan 
+$nilai = 70;
+
+if($nilai >= 85){
+    echo "Grade A";
+} else if($nilai >= 70){
+    echo "Grade B";
+} else if($nilai >=50){
+    echo "Grade C";
+} else if($nilai >=30){
+    echo "Grade D";
+} else {
+    echo "Grade E";
+}
+
+
 ?>
