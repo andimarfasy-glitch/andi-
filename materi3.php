470811ea3210e3ce9d1eb7fa09708f2e9a389d05
andimarfasy-glitch <andimarfasy@gmail.com>
2026-04-27 12:14:22 +0800

    materi3.php

--- materi3.php
+++ materi3.php
@@ -1,17 +1,52 @@
-<form method= "POST">
-    Masukkan angka : <input type="number" name="angka">
+<?php
+   function hello()
+   {
+    echo "selamat datang";
+   }
+   hello();
+
+   function tambah(int $a,int $b){
+     $hasil= $a + $b; 
+     return $hasil;
+   }
+   echo "<br><br>";
+  echo tambah(12,8);
+
+  function kali(int $a,int $b){
+     $hasil= $a * $b; 
+     return $hasil;
+   }
+   
+   function bagi(int $a,int $b){
+     $hasil= $a / $b; 
+     return $hasil;
+   }
+
+   function kurang(int $a,int $b){
+     $hasil= $a - $b; 
+     return $hasil;
+   }
+   
+  
+
+   ?>
+
+  <form method= "post">
+    Masukkan angka 1 : <input type="number" name="angka1">
+    Masukkan angka 2 : <input type="number" name="angka2">
     <input type="submit" name="kirim" value="kirim">
 </form>
 
 <?php
-  if(isset($_POST["angka"])){
-    $newAngka = $_POST["angka"];
-    for($i=1; $i<=$newAngka;$i++){
-        if($i % 2 != 0){
-        echo "ini ganjil $i <br>";
-    }  else {
-        echo "ini genap $i <br>"; 
-    }
-  }
+  if(isset($_POST["angka1"])){
+    $newangka1 = $_POST["angka1"];
+    $newangka2 = $_POST["angka2"];
+    echo tambah($newangka1,$newangka2);
+    echo"<br>";
+    echo kali($newangka1,$newangka2);
+     echo"<br>";
+    echo bagi($newangka1,$newangka2);
+     echo"<br>";
+    echo kurang($newangka1,$newangka2);
   }
 ?>
